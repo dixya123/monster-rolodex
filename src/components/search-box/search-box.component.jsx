685af7f3c7923435687bd6691f905780df463663
dixya123 <dixyamaharjan@sxc.edu.np>
2021-06-07 12:36:50 +0545

    finished

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import './search-box.style.css';
+export const SearchBox=({placeholder,handleChange})=>(
+    <input type='search' className='search' placeholder={placeholder} onChange={handleChange}/>
+);
